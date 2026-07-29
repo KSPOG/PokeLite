2c9da16881daffbf260cd16c7c7a4afa3bc071ce
KSPOG <kratosmaximusosrs@gmail.com>
2026-07-29 17:39:39 +0200

    Bootstrap shared client and plugin APIs

--- src/main/java/dev/kspog/pokelite/PokeLite.java
+++ src/main/java/dev/kspog/pokelite/PokeLite.java
@@ -1,5 +1,8 @@
 package dev.kspog.pokelite;
 
+import dev.kspog.pokelite.core.DefaultClientApi;
+import dev.kspog.pokelite.core.DefaultEventBus;
+import dev.kspog.pokelite.core.DefaultPluginContext;
 import dev.kspog.pokelite.plugin.PluginManager;
 import dev.kspog.pokelite.ui.PokeLiteFrame;
 import dev.kspog.pokelite.ui.UiTheme;
@@ -25,8 +28,25 @@ public final class PokeLite {
             try {
                 UiTheme.install();
                 Path projectRoot = Paths.get(System.getProperty("user.dir")).toAbsolutePath().normalize();
-                PluginManager pluginManager = new PluginManager(projectRoot.resolve("plugins"));
-                PokeLiteFrame frame = new PokeLiteFrame(projectRoot, pluginManager);
+
+                DefaultEventBus eventBus = new DefaultEventBus();
+                DefaultClientApi clientApi = new DefaultClientApi(eventBus);
+                DefaultPluginContext pluginContext = new DefaultPluginContext(
+                    clientApi,
+                    eventBus,
+                    projectRoot
+                );
+                PluginManager pluginManager = new PluginManager(
+                    projectRoot.resolve("plugins"),
+                    pluginContext
+                );
+
+                PokeLiteFrame frame = new PokeLiteFrame(
+                    projectRoot,
+                    pluginManager,
+                    clientApi,
+                    eventBus
+                );
                 frame.setVisible(true);
                 frame.startClient();
             } catch (RuntimeException error) {
